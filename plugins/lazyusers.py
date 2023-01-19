6877e9db81544b6b234bbd3df9f6ddb909c8fb5f
LieStage <118051769+LieStage@users.noreply.github.com>
2023-01-19 14:42:16 +0530

    Update lazyusers.py

--- plugins/lazyusers.py
+++ plugins/lazyusers.py
@@ -1,9 +1,9 @@
 import os 
 from pyrogram import Client, filters
 from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup)
-token = os.environ.get('TOKEN','')
+token = os.environ.get('TOKEN','5793472760:AAE2N3-RFo0V-8S87fke4iCROCWjRS3kWrc')
 botid = token.split(':')[0]
-ADMIN = int(os.environ.get("ADMIN", ""))
+ADMIN = int(os.environ.get("ADMIN", "945284066"))
 
 from helper.database import botdata, find_one, total_user,getid
 
